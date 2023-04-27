56f4dc4babaff357d448a213212091b282547ac1
retomike <miguelangelluisespinoza@gmail.com>
2023-04-26 23:31:09 -0600

    Botones sencillos

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'My App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  //Do function
+                },
+                child: Text('Botón 1'),
+              ),
+              SizedBox(height: 16), // Espacio entre los botones
+              ElevatedButton(
+                onPressed: () {
+                  print('¡Hola!');
+                },
+                child: Text('Botón 2'),
+              ),
+               SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  //Do function
+                },
+                child: Text('Botón 3'),
+              ),
+               SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  //Do function
+                },
+                child: Text('Botón 4'),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
