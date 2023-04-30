8a9219aff666ba33e70aec4a7c183e089fd81227
Gabriel Angeles <bonnesbaby14@gmail.com>
2023-04-30 00:16:39 -0600

    nuevo diseño de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,253 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MaterialApp(
-    title: 'iDrink',
-    home: const MyApp(),
-  ));
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Roboto'),
+      home: HomePage(key: Key('myHomePage')),
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
       appBar: AppBar(
-        title: const Text('iDrink'),
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                final response = await http.get(Uri.parse(
-                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/greyhound'));
-                if (response.statusCode == 200) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Sirviendo...')));
-                }else{
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('El servicio no esta disponible...')));
-                }
-              },
-              child: const Text('Greyhound'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                final response = await http.get(Uri.parse(
-                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/tequila_sunrise'));
-                if (response.statusCode == 200) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Sirviendo...')));
-                }else{
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('El servicio no esta disponible...')));
-                }
-              },
-              child: const Text('Tequila Sunrise'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                final response = await http.get(Uri.parse(
-                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/desarmador'));
-                if (response.statusCode == 200) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Sirviendo...')));
-                }else{
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('El servicio no esta disponible...')));
-                }
-              },
-              child: const Text('Desarmador'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                final response = await http.get(Uri.parse(
-                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/cosmopolitan'));
-                if (response.statusCode == 200) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Sirviendo...')));
-                }else{
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('El servicio no esta disponible...')));
-                }
-              },
-              child: const Text('Cosmopolitan'),
-            ),
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(30))),
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Find Your',
+                      style: TextStyle(color: Colors.black87, fontSize: 25),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Drink',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(244, 243, 243, 1),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.black87,
+                            ),
+                            hintText: "Search you're looking for",
+                            hintStyle:
+                                TextStyle(color: Colors.grey, fontSize: 15)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Promo Today',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () async {
+                              // Agrega aquí la acción que deseas realizar
+                              final response = await http.get(Uri.parse(
+                                  'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/greyhound'));
+                              if (response.statusCode == 200) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Sirviendo...')));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'El servicio no esta disponible...')));
+                              }
+                            },
+                            child: promoCard('assets/images/one.jpg'),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              final response = await http.get(Uri.parse(
+                                  'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/tequila_sunrise'));
+                              if (response.statusCode == 200) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Sirviendo...')));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'El servicio no esta disponible...')));
+                              }
+                            },
+                            child: promoCard('assets/images/two.jpg'),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              // Agrega aquí la acción que deseas realizar
+                              final response = await http.get(Uri.parse(
+                                  'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/desarmador'));
+                              if (response.statusCode == 200) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Sirviendo...')));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'El servicio no esta disponible...')));
+                              }
+                            },
+                            child: promoCard('assets/images/three.jpg'),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              // Agrega aquí la acción que deseas realizar
+                              final response = await http.get(Uri.parse(
+                                  'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve/cosmopolitan'));
+                              if (response.statusCode == 200) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Sirviendo...')));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'El servicio no esta disponible...')));
+                              }
+                            },
+                            child: promoCard('assets/images/four.jpg'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/five.jpg')),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: [
+                                0.3,
+                                0.9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(.2)
+                              ]),
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                              'Best Flavour',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget promoCard(image) {
+    return AspectRatio(
+      aspectRatio: 2.62 / 3,
+      child: Container(
+        margin: EdgeInsets.only(right: 15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+                0.1,
+                0.9
+              ], colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.1)
+              ])),
         ),
       ),
     );
