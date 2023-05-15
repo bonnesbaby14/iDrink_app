8aeb9658eec51f6fdb79840936f675fe677e3ba5
bonnesbaby14 <bonnesbaby14@gmail.com>
2023-05-14 18:03:18 -0600

    ajuste de inciio y sesion

--- lib/main.dart
+++ lib/main.dart
@@ -1,300 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Roboto'),
-      home: HomePage(key: Key('myHomePage')),
-    ));
+import 'onboding_screen.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-  @override
-  _HomePageState createState() => _HomePageState();
+
+
+void main() {
+  runApp(const MyApp());
 }
 
-class _HomePageState extends State<HomePage> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.black87,
-          ),
-          onPressed: () {},
+    return MaterialApp(
+      title: 'The Flutter Way',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFEEF1F8),
+        primarySwatch: Colors.blue,
+        fontFamily: "Intel",
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
         ),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:
-                        BorderRadius.vertical(bottom: Radius.circular(30))),
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Find Your',
-                      style: TextStyle(color: Colors.black87, fontSize: 25),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      'Drink',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(244, 243, 243, 1),
-                          borderRadius: BorderRadius.circular(15)),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Colors.black87,
-                            ),
-                            hintText: "Search you're looking for",
-                            hintStyle:
-                                TextStyle(color: Colors.grey, fontSize: 15)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Promo Today',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      height: 200,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () async {
-                              // Agrega aquí la acción que deseas realizar
-                              final response = await http.post(
-                                Uri.parse(
-                                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve'),
-                                headers: {
-                                  'Content-Type': 'application/json',
-                                },
-                                body: jsonEncode({
-                                  'drink': 'greyhound',
-                                  'user': 'tu_usuario'
-                                }),
-                              );
-
-                              print(response.statusCode);
-                              print(response.body);
-                              if (response.statusCode == 200) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text('Sirviendo...')));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text(
-                                            'El servicio no esta disponible...')));
-                              }
-                            },
-                            child: promoCard('assets/images/one.jpg'),
-                          ),
-                          GestureDetector(
-                            onTap: () async {
-                              final response = await http.post(
-                                Uri.parse(
-                                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve'),
-                                headers: {
-                                  'Content-Type': 'application/json',
-                                },
-                                body: jsonEncode({
-                                  'drink': 'tequila_sunrise',
-                                  'user': 'tu_usuario'
-                                }),
-                              );
-
-                              
-                              if (response.statusCode == 200) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text('Sirviendo...')));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text(
-                                            'El servicio no esta disponible...')));
-                              }
-                            },
-                            child: promoCard('assets/images/two.jpg'),
-                          ),
-                          GestureDetector(
-                            onTap: () async {
-                              // Agrega aquí la acción que deseas realizar
-                              final response = await http.post(
-                                Uri.parse(
-                                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve'),
-                                headers: {
-                                  'Content-Type': 'application/json',
-                                },
-                                body: jsonEncode({
-                                  'drink': 'desarmador',
-                                  'user': 'tu_usuario'
-                                }),
-                              );
-
-                             
-                              if (response.statusCode == 200) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text('Sirviendo...')));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text(
-                                            'El servicio no esta disponible...')));
-                              }
-                            },
-                            child: promoCard('assets/images/three.jpg'),
-                          ),
-                          GestureDetector(
-                            onTap: () async {
-                              // Agrega aquí la acción que deseas realizar
-                              final response = await http.post(
-                                Uri.parse(
-                                    'https://idrink-api-prod-idrink-api-fqemyp.mo2.mogenius.io/serve'),
-                                headers: {
-                                  'Content-Type': 'application/json',
-                                },
-                                body: jsonEncode({
-                                  'drink': 'cosmopolitan',
-                                  'user': 'tu_usuario'
-                                }),
-                              );
-
-                              
-                              if (response.statusCode == 200) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text('Sirviendo...')));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text(
-                                            'El servicio no esta disponible...')));
-                              }
-                            },
-                            child: promoCard('assets/images/four.jpg'),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('assets/images/five.jpg')),
-                      ),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          gradient: LinearGradient(
-                              begin: Alignment.bottomRight,
-                              stops: [
-                                0.3,
-                                0.9
-                              ],
-                              colors: [
-                                Colors.black.withOpacity(.8),
-                                Colors.black.withOpacity(.2)
-                              ]),
-                        ),
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Text(
-                              'Best Flavour',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget promoCard(image) {
-    return AspectRatio(
-      aspectRatio: 2.62 / 3,
-      child: Container(
-        margin: EdgeInsets.only(right: 15.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-                0.1,
-                0.9
-              ], colors: [
-                Colors.black.withOpacity(.8),
-                Colors.black.withOpacity(.1)
-              ])),
-        ),
-      ),
+      home: const OnbodingScreen(),
     );
   }
 }
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color(0xFFDEE3F2),
+    width: 1,
+  ),
+);
